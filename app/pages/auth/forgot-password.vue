bc65da84343088e78566886d6ef846a7d9ca2b27
Linus Fischer <leicraft@leicraftmc.de>
2025-12-12 00:49:44 +0100

    update

--- app/pages/auth/forgot-password.vue
+++ app/pages/auth/forgot-password.vue
@@ -87,7 +87,7 @@ async function onSubmit(payload: FormSubmitEvent<Schema>) {
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center gap-4 p-4">
+    <div class="flex min-h-full items-center justify-center p-4">
         <UPageCard class="w-full max-w-md">
             <UAuthForm :schema="schema" title="Login" description="Enter your credentials to access your account."
                 icon="i-lucide-user" :fields="fields" @submit="onSubmit" 
