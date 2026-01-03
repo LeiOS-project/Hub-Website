893024491f76c334b7fbab135485398f4466c00d
Linus Fischer <leicraft@leicraftmc.de>
2026-01-03 01:58:05 +0000

    Neue Datei hinzufügen

---
+++ app/components/Gravatar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { AvatarProps } from '@nuxt/ui';
+import { useGravatarURL } from '~/composables/useGravatarURL';
+
+const props = defineProps<Omit<AvatarProps, 'src'> & { email: string }>();
+
+const avatarSrc = await useGravatarURL(props.email);
+
+</script>
+
+<template>
+    <UAvatar
+        :src="avatarSrc"
+        v-bind="props"
+    >
+        <slot />
+    </UAvatar>
+</template>
+
