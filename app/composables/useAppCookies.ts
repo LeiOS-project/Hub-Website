eb53cd9028e2ed6646a33bca9370d454652259ab
Linus Fischer <leicraft@leicraftmc.de>
2026-01-09 11:51:32 +0000

    updated cookie handling

--- app/composables/useAppCookies.ts
+++ app/composables/useAppCookies.ts
@@ -26,10 +26,22 @@ class AppCookie<T extends string | null | undefined> {
 
 }
 
-export function useAppCookies() {
-    
-    return {
+class AppCookieHandler {
+
+    private constructor() {}
+
+    private static readonly cookies = {
+
         sessionToken: new AppCookie<string | null>("leioshub_session_token"),
+        
+    } as const;
+
+    static getCookies() {
+        return AppCookieHandler.cookies;
     }
 
+}
+
+export function useAppCookies() {
+    return AppCookieHandler.getCookies();
 }
